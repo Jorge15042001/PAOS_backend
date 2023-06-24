d49948d20a52223164f300396ecdcae445e4e3a5
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-06-23 23:08:41 -0500

    new model for cart

--- PAOS/app/models.py
+++ PAOS/app/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()
     deleted = models.BooleanField(default=False)
     available = models.BooleanField(default=True)
 
+
 class ProductCharacteristic(models.Model):
     value = models.CharField(max_length=100)
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="characteristics")
+    product = models.ForeignKey(
+        "Product", on_delete=models.CASCADE, related_name="characteristics")
+
+
+class CartProduct(models.Model):
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="product")
+    client = models.ForeignKey(
+        "accounts.PAOSUser", on_delete=models.CASCADE, related_name="client")

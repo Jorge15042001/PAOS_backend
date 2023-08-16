09b74f50f40d5f37aadb2686f5b7aa68c66db2a8
Jorge10542001 <adrivuglarin@hotmail.com>
2023-08-16 00:41:13 -0500

    all urls must end with /

--- PAOS/accounts/urls.py
+++ PAOS/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api_login",views.APILogin.as_view(),name="api_login"),
-    path("api_signup",views.APISignup.as_view(),name="api_signup")
+    path("api_login/",views.APILogin.as_view(),name="api_login"),
+    path("api_signup/",views.APISignup.as_view(),name="api_signup")
 ]
 
--- PAOS/app/urls.py
+++ PAOS/app/urls.py
@@ -4,19 +4,19 @@ from . import views
 
 urlpatterns = [
     path("product/", views.ProductAPI.as_view(), name="product"),
-    path("product/<int:product_id>", views.ProductDetailAPI.as_view(),
+    path("product/<int:product_id>/", views.ProductDetailAPI.as_view(),
          name="product_deltail"),
     path("characteristic/", views.ProductCharacteristicAPI.as_view(),
          name="product_characteristic"),
-    path("characteristic/<int:characteristic_id>", views.ProductCharacteristicDetailAPI.as_view(),
+    path("characteristic/<int:characteristic_id>/", views.ProductCharacteristicDetailAPI.as_view(),
          name="product_characteristic_detail"),
     path("cart/", views.ProductCartAPI.as_view(),
          name="product_cart"),
-    path("cart/<int:item_id>", views.ProductCartDetailAPI.as_view(),
+    path("cart/<int:item_id>/", views.ProductCartDetailAPI.as_view(),
          name="product_cart_detail"),
     path("order/", views.OrderAPI.as_view(), name="order"),
-    path("order/<int:order_id>", views.OrderDetailAPI.as_view(), name="order_detail"),
-    path("order_deliverer/<int:order_id>", views.OrderDetailAPI.as_view(), name="order_deliverer"),
+    path("order/<int:order_id>/", views.OrderDetailAPI.as_view(), name="order_detail"),
+    path("order_deliverer/<int:order_id>/", views.OrderDetailAPI.as_view(), name="order_deliverer"),
     path("order_cart/", views.OrderCartAPI.as_view(), name="order_cart"),
 
 ]
